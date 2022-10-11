51862785980c4122012ed23939c9d992d4fc6b28
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-11 19:00:08 -0400

    Selection Sort Solution Review in main()

--- SelectionSortReview/main.cpp
+++ SelectionSortReview/main.cpp
@@ -1,14 +1,42 @@
-//
-//  main.cpp
-//  SelectionSortReview
-//
-//  Created by Matt Ahmad on 2022-10-11.
-//
-
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int i, j, n, loc, temp, min, a[30];
+    cout<<"Enter the number of elements:";
+    cin>>n;
+    cout<<"\nEnter the elements:";
+    
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        min=a[i];
+        loc=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(min>a[j])
+            {
+                min=a[j];
+                loc=j;
+            }
+        }
+        
+        temp=a[i];
+        a[i]=a[loc];
+        a[loc]=temp;
+    }
+    
+    cout<<"\nSorted list is as follows\n";
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
     return 0;
 }
